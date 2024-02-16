13511f07249830c2762ae942eb2d3cfba2f3dc11
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-02-16 20:31:29 +0100

    React Basics week 1 - completed

---
+++ Course 5 - React Basics/Week 1 - React Components/labs/3-passing-props/src/App.js
@@ -0,0 +1,13 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Bob" />
+            <Heading firstName="Ginny" />
+            <Heading firstName="Thomas" />
+        </div>
+    );
+};
+
+export default App;
