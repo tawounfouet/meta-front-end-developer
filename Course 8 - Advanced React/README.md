ef4209996a2843a6fc92ed0a34a13735f8606060
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-02-17 11:34:40 +0100

    Advanced react - Week 1:Components

---
+++ Course 8 - Advanced React/README.md
@@ -0,0 +1,18 @@
+# Advanced React
+
+<img src="../logo.avif">
+
+- This directory contains all of my assignments from the Coursera Course: [Advanced React](https://www.coursera.org/learn/advanced-react)
+
+## Table of Content
+
+  - Week 1: [Components](https://github.com/x39OME/Meta-Front-End-Developer-Professional-Certificate/tree/main/6%20-%20Advanced%20React/Week%201%20-%20Components)
+  - Week 2: [React Hooks and Custom Hooks](https://github.com/x39OME/Meta-Front-End-Developer-Professional-Certificate/tree/main/6%20-%20Advanced%20React/Week%202%20-%20React%20Hooks%20and%20Custom%20Hooks)
+  - Week 3: [JSX and testing](https://github.com/x39OME/Meta-Front-End-Developer-Professional-Certificate/tree/main/6%20-%20Advanced%20React/Week%203%20-%20JSX%20and%20testing)
+  - Week 4: [Final project](https://github.com/x39OME/Meta-Front-End-Developer-Professional-Certificate/tree/main/6%20-%20Advanced%20React/Week%204%20-%20Final%20project)
+
+## Proof of Completion
+
+<a href="https://www.coursera.org/account/accomplishments/verify/38FN3SD2ZPTZ"> Verified Coursera Certifies</a>
+
+<img src="./certificate.png" alt="certificate">
---
+++ Course 8 - Advanced React/Week 1 - Components/lab/1.create-a-basic-list-component/src/DessertsList-v2.js
@@ -0,0 +1,38 @@
+function DessertsList(props) {
+    // Implement the component here.
+    const lowCaloriesDesserts = props.data
+      
+      // Filtering
+      // The filter method creates a copy of the array, filtered down to just the elements from the original array that pass the test.
+      // In order words, it will return a new list with just the elements that fulfil the condition.
+      .filter((dessert) => {
+        return dessert.calories < 500;
+      })
+  
+      // Sorting
+      // JavaScript offer the sort() method, which sorts the elements of an array based on a comparison function provided
+  
+      .sort((a, b) => { 
+       return a.calories - b.calories; 
+      })
+  
+      // Mapping
+      // , to apply the desired projection and display the information as requested, 
+      // you can chain the map operator at the end and return a < li > item with the dessert name and its calories,
+      .map((dessert) => { 
+       return ( 
+         <li>
+           {dessert.name} - {dessert.calories} cal
+         </li> 
+       ); 
+      
+      });
+   
+     
+    //return null;
+    return <ul>{lowCaloriesDesserts}</ul>;
+    
+  }
+  
+  export default DessertsList;
+  
